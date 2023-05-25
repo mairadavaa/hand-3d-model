07beae5965e75cf4c4366194a5fc0646b4df8509
maira <davaamaira@gmail.com>
2023-05-25 13:31:55 +0800

    toggleColor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,33 @@
 import { OrbitControls } from '@react-three/drei';
-
+import { useState } from "react"
 import { Canvas } from 'react-three-fiber';
 import { Model } from '../../public/Model';
 import { GreenModel } from '../../public/GreenModel'
 import { PinkModel } from '../../public/PinkModel';
 export default function Home() {
+  const [toggle, setToggle] = useState(2)
+
+  const toggleButton = (index) => {
+      setToggle(index)
+      console.log(index)
+  }
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <div> fkdhhkdahkfhk</div>
+      <div style={{display:"flex", flexDirection:"row", gap:"10px"}}>
+        <div> Choose color</div>
+
+      <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"brown"}} onClick={() => toggleButton(1)}></div>
+      <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"green"}} onClick={() => toggleButton(2)}></div>
+      <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"pink"}} onClick={() => toggleButton(3)}></div>
+      </div>
+      
       <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 10, 0]} intensity={0.5} />
-        <Model />
+      {toggle ===1 ?  <Model /> : <GreenModel />}
+      {toggle ===2 ? <GreenModel />: <GreenModel />}
+      {toggle ===3 ?  <PinkModel />: <GreenModel />}
+       
        
         <OrbitControls
           enableZoom={true}
@@ -23,38 +39,7 @@ export default function Home() {
         // rotateSpeed={0.5}
         />
       </Canvas>
-      <div> fkdhhkdahkfhk</div>
-      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 0]} intensity={0.5} />
-        <GreenModel />
-       
-        <OrbitControls
-          enableZoom={true}
-        // minPolarAngle={1.45}
-        // maxPolarAngle={Math.PI / 1.85}
-        // autoRotate
-        // enableRotate={true}
-        // autoRotateSpeed={5}
-        // rotateSpeed={0.5}
-        />
-      </Canvas>
-      <div> fkdhhkdahkfhk</div>
-      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 10, 0]} intensity={0.5} />
-        <PinkModel />
-       
-        <OrbitControls
-          enableZoom={true}
-        // minPolarAngle={1.45}
-        // maxPolarAngle={Math.PI / 1.85}
-        // autoRotate
-        // enableRotate={true}
-        // autoRotateSpeed={5}
-        // rotateSpeed={0.5}
-        />
-      </Canvas>
+
     </div>
   );
 }
