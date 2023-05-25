e8e660e2e5dd38c7396ef352a122afaee2492528
maira <davaamaira@gmail.com>
2023-05-25 13:38:07 +0800

    changeStyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Model } from '../../public/Model';
 import { GreenModel } from '../../public/GreenModel'
 import { PinkModel } from '../../public/PinkModel';
 export default function Home() {
-  const [toggle, setToggle] = useState(2)
+  const [toggle, setToggle] = useState(1)
 
   const toggleButton = (index) => {
       setToggle(index)
@@ -13,15 +13,15 @@ export default function Home() {
   }
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <div style={{display:"flex", flexDirection:"row", gap:"10px"}}>
+      <div style={{display:"flex", flexDirection:"row", gap:"10px", marginTop:"50px"}}>
         <div> Choose color</div>
 
-      <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"brown"}} onClick={() => toggleButton(1)}></div>
+      <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"#D19D75"}} onClick={() => toggleButton(1)}></div>
       <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"green"}} onClick={() => toggleButton(2)}></div>
       <div style={{ width: "20px", height: "20px", borderRadius: "50%", backgroundColor:"pink"}} onClick={() => toggleButton(3)}></div>
       </div>
       
-      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
+      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black", marginTop:"20px"}} >
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 10, 0]} intensity={0.5} />
       {toggle ===1 ?  <Model /> : <GreenModel />}
