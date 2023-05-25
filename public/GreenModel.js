6e8b3a9fcd6ea78bb205ef43b5724e27d86df737
maira <davaamaira@gmail.com>
2023-05-25 12:56:48 +0800

    add hand

---
+++ public/GreenModel.js
@@ -0,0 +1,22 @@
+
+
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function GreenModel(props) {
+  const { nodes, materials } = useGLTF("/hand2green.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={materials.Material}
+        position={[-0.22, 0.18, -0.02]}
+        scale={[1.34, 1.8, 0.45]}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/hand2green.glb");
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,16 @@ import { OrbitControls } from '@react-three/drei';
 
 import { Canvas } from 'react-three-fiber';
 import { Model } from '../../public/Model';
-
+import { GreenModel } from '../../public/GreenModel'
 export default function Home() {
   return (
-    <div style={{ width: "100%", height: "1000px" }}>
+    <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "row" }}>
       <div> fkdhhkdahkfhk</div>
-      <Canvas style={{ width:"600px", height:"1000px", border:"1px solid black"}} >
+      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 10, 0]} intensity={0.5} />
         <Model />
+       
         <OrbitControls
           enableZoom={true}
         // minPolarAngle={1.45}
@@ -21,7 +22,22 @@ export default function Home() {
         // rotateSpeed={0.5}
         />
       </Canvas>
-     
+      <div> fkdhhkdahkfhk</div>
+      <Canvas style={{ width:"40%", height:"50vh", border:"1px solid black"}} >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 10, 0]} intensity={0.5} />
+        <GreenModel />
+       
+        <OrbitControls
+          enableZoom={true}
+        // minPolarAngle={1.45}
+        // maxPolarAngle={Math.PI / 1.85}
+        // autoRotate
+        // enableRotate={true}
+        // autoRotateSpeed={5}
+        // rotateSpeed={0.5}
+        />
+      </Canvas>
   
     </div>
   );
